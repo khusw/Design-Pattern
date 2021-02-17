26b11db67306b88e559ec24d7ddb7ce0a483c94f
sdy <vel1024@khu.ac.kr>
2021-02-17 23:25:47 +0900

    add comments

--- Strategy-Pattern/src/com/sp/inter/Main.java
+++ Strategy-Pattern/src/com/sp/inter/Main.java
@@ -1,6 +1,7 @@
-package com.sp.main;
+package com.sp.inter;
 
 public class Main {
+    // 구현과 선언을 따로 함과 동시에 기능을 실행하기 위한 통로 역할을 수행
     public static void main(String[] args) {
         AInterface aInterface = new AInterfaceImpl();
         aInterface.funcA();

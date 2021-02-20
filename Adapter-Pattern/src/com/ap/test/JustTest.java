c5ed5fa8a99af182fa0b40b330e806702f60180a
sdy <vel1024@khu.ac.kr>
2021-02-20 23:06:02 +0900

    remove test file

--- Adapter-Pattern/src/com/ap/test/JustTest.java
+++
@@ -1,7 +0,0 @@
-package com.ap.test;
-
-public class JustTest {
-    public static void main(String[] args) {
-        System.out.println("Just testing method for previewing readme.md in github");
-    }
-}

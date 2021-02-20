8ba8b29f3f37ea87db45b4649044ce5cf3b00cc6
sdy <vel1024@khu.ac.kr>
2021-02-20 23:04:31 +0900

    add test file

---
+++ Adapter-Pattern/src/com/ap/test/JustTest.java
@@ -0,0 +1,7 @@
+package com.ap.test;
+
+public class JustTest {
+    public static void main(String[] args) {
+        System.out.println("Just testing method for previewing readme.md in github");
+    }
+}
